07026862dabefc05b1cae3f8c4c33530c25581f5
Sujith6869 <118263472+Sujith6869@users.noreply.github.com>
2022-11-23 07:22:12 -0800

    program

---
+++ class selector.java
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <style>
+        .class{
+            text-align: center;
+            color:red
+
+        }
+        </style>
+    </head>
+    <body>
+        <p class "class">welcome to css</p>
+    </body>
+</html>
